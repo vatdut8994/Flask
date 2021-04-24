9d4b8fd1b7b904cd7ccb5fdecba8ed47bfab3137
vatdut8994 <82714982+vatdut8994@users.noreply.github.com>
2021-04-24 10:17:13 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route("/")
+def connectivity():
+    return render_template("untitled.html")
+@app.route("/detail",methods=['GET','POST'])
+def summision():
+    if(request.method=='POST'):
+        a=int(request.form['a'])
+        b=int(request.form['b'])
+        total=a+b
+        return render_template('untitled.html',card=total)
+if __name__=="__main__":
+    app.run(debug=True)
